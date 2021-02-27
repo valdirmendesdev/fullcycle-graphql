b62dbd05e6a5da796460b0255bae9f7d6f88ab1d
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-02-27 11:27:52 -0300

    Implementing lazy loading + relationships

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -12,8 +12,26 @@ import (
 	"github.com/valdirmendesdev/fc2-graphql/graph/model"
 )
 
-func genID() string {
-	return fmt.Sprintf("T%d", rand.Int())
+func (r *categoryResolver) Courses(ctx context.Context, obj *model.Category) ([]*model.Course, error) {
+	var courses []*model.Course
+
+	for _, course := range r.Resolver.Courses {
+		if course.Category.ID == obj.ID {
+			courses = append(courses, course)
+		}
+	}
+	return courses, nil
+}
+
+func (r *courseResolver) Chapters(ctx context.Context, obj *model.Course) ([]*model.Chapter, error) {
+	var chapters []*model.Chapter
+
+	for _, chapter := range r.Resolver.Chapters {
+		if chapter.Course.ID == obj.ID {
+			chapters = append(chapters, chapter)
+		}
+	}
+	return chapters, nil
 }
 
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
@@ -37,10 +55,10 @@ func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCour
 	}
 
 	course := model.Course{
-		ID:       genID(),
+		ID:          genID(),
 		Description: &input.Description,
-		Name:     input.Name,
-		Category: category,
+		Name:        input.Name,
+		Category:    category,
 	}
 
 	r.Courses = append(r.Courses, &course)
@@ -58,8 +76,8 @@ func (r *mutationResolver) CreateChapter(ctx context.Context, input model.NewCha
 	}
 
 	chapter := model.Chapter{
-		ID:       genID(),
-		Name:     input.Name,
+		ID:     genID(),
+		Name:   input.Name,
 		Course: course,
 	}
 
@@ -80,11 +98,29 @@ func (r *queryResolver) Chapters(ctx context.Context) ([]*model.Chapter, error) 
 	return r.Resolver.Chapters, nil
 }
 
+// Category returns generated.CategoryResolver implementation.
+func (r *Resolver) Category() generated.CategoryResolver { return &categoryResolver{r} }
+
+// Course returns generated.CourseResolver implementation.
+func (r *Resolver) Course() generated.CourseResolver { return &courseResolver{r} }
+
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+type categoryResolver struct{ *Resolver }
+type courseResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+func genID() string {
+	return fmt.Sprintf("T%d", rand.Int())
+}
